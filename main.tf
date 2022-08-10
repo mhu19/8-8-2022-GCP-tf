a1026bd81d4ad7829592b173e96b1b5435f1030e
mhu19 <46738420+mhu19@users.noreply.github.com>
2022-08-10 08:35:24 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,41 +8,10 @@ terraform {
 }
 
 provider "google" {
-  credentials = file(var.credentials_file)
 
   project = var.project
   region  = var.region
   zone    = var.zone
 
-}
-
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
-}
-
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-vm-instance"
-  machine_type = "f1-micro"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-
-  network_interface {
-    network = google_compute_network.vpc_network.name
-    access_config {
-    }
-  }
-}
-
-resource "google_sql_database_instance" "mysql_instance" {
-  name             = "terraform-mysql-instance1"
-  region           = "us-central1"
-  database_version = "MYSQL_5_7"
-  settings {
-    tier = "db-f1-micro"
-  }  
 }
 

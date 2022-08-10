303d961065fa48955dfd0c25d33a2b3593df54df
mhu19 <46738420+mhu19@users.noreply.github.com>
2022-08-10 13:52:50 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,30 +5,9 @@ provider "google" {
   zone    = var.zone
 }
 
-module "bastion_host" {
-  source = "terraform-google-modules/bastion-host/google//modules/bastion-group"
-  
-  project = var.project
-  region  = var.region
-  zone    = var.zone
-  
-  network = google_compute_network.vpc_network.self_link
-  subnet = google_compute_subnetwork.subnet.self_link
-  
-}
-  
 resource "google_compute_network" "vpc_network" {
-  name = "terraform-vpcnetwork"
-  auto_create_subnetworks = false
+  name = "terraform-network"
 }
-
-resource "google_compute_subnetwork" "subnet" {
-  name = "terraform-subnet"
-  ip_cidr_range = "10.127.0.0/20"
-  network = google_compute_network.vpc_network.self_link
-  private_ip_google_access = true
-}
-
 
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-vm-instance"

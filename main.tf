bdfcdfd8f4c13d2ad11e1f1e6200c9f58bbb0f00
mhu19 <46738420+mhu19@users.noreply.github.com>
2022-08-18 23:04:56 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
-
 provider "google" {
   project = var.project
   region  = var.region
   zone    = var.zone
 }
 
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
 
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-vm-instance"
@@ -17,10 +19,8 @@ resource "google_compute_instance" "vm_instance" {
   }
 
   network_interface {
-    network = "default"
-
+    network = google_compute_network.vpc_network.name
     access_config {
-      // Ephemeral public IP
     }
   }
 }
